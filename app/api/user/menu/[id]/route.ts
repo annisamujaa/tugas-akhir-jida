73616ac7ef3980978e867baa52b15cd05aca7fa9
annisamujaa <annisama2611@gmail.com>
2025-08-01 12:01:41 +0700

    edit menu id routes

--- app/api/user/menu/[id]/route.ts
+++ app/api/user/menu/[id]/route.ts
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from '@/lib/prisma';
 
-export async function GET(request: NextRequest) {
-  const url = new URL(request.url);
-  const id = url.pathname.split('/').pop(); // Ambil ID dari URL
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
 
   if (!id) {
     return NextResponse.json({ error: "ID tidak valid" }, { status: 400 });
