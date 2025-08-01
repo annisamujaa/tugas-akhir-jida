f21aa6ccca8192c49643299d1aa3ed06b88bc9ed
annisamujaa <annisama2611@gmail.com>
2025-08-01 21:03:16 +0700

    next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -111,6 +111,4 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+export const { handlers: { GET, POST }, auth } = NextAuth(authOptions);
