a1ea714a2cd932bbb2878eb354e84b760f02f956
Annisa Muja Ahidah <80079890+annisamujaa@users.noreply.github.com>
2025-08-01 12:14:14 +0700

    Update route.ts

--- app/api/user/menu/[id]/route.ts
+++ app/api/user/menu/[id]/route.ts
@@ -1,24 +1,27 @@
+// app/api/admin/menu/[id]/route.ts
+
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from '@/lib/prisma';
+import { prisma } from "@/lib/prisma";
 
 export async function GET(request: NextRequest) {
-  const url = request.nextUrl;
-  const id = url.pathname.split("/").pop(); // ambil [id] dari URL path
+  const id = request.nextUrl.pathname.split("/").pop(); // ambil id dari URL
 
   if (!id) {
     return NextResponse.json({ error: "ID tidak valid" }, { status: 400 });
   }
 
   try {
-    const menu = await prisma.menu.findUnique({ where: { id } });
+    const menu = await prisma.menu.findUnique({
+      where: { id },
+    });
 
     if (!menu) {
-      return NextResponse.json({ error: 'Menu tidak ditemukan' }, { status: 404 });
+      return NextResponse.json({ error: "Menu tidak ditemukan" }, { status: 404 });
     }
 
     return NextResponse.json(menu);
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: 'Gagal mengambil menu' }, { status: 500 });
+    return NextResponse.json({ error: "Gagal mengambil menu" }, { status: 500 });
   }
 }
