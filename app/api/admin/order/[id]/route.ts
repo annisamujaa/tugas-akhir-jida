e5d4d970c4fe1129a56d01e6fe581126666f9c89
annisamujaa <annisama2611@gmail.com>
2025-08-01 20:27:24 +0700

    get order admin 1 params

--- app/api/admin/order/[id]/route.ts
+++ app/api/admin/order/[id]/route.ts
@@ -1,11 +1,43 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+// // GET detail order
+// export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+//   try {
+//     const order = await prisma.order.findUnique({
+//       where: { id: params.id },
+//       include: {
+//         items: {
+//           include: {
+//             menu: true,
+//           },
+//         },
+//       },
+//     });
+
+//     if (!order) {
+//       return NextResponse.json({ error: "Order tidak ditemukan" }, { status: 404 });
+//     }
+
+//     return NextResponse.json(order);
+//   } catch (error: any) {
+//     console.error("Gagal ambil detail order:", error.message);
+//     return NextResponse.json({ error: "Server error", detail: error.message }, { status: 500 });
+//   }
+// }
+
+
 // GET detail order
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(req: NextRequest) {
+  const id = req.nextUrl.pathname.split("/").pop();
+
+  if (!id) {
+    return NextResponse.json({ error: "ID tidak ditemukan" }, { status: 400 });
+  }
+
   try {
     const order = await prisma.order.findUnique({
-      where: { id: params.id },
+      where: { id },
       include: {
         items: {
           include: {
