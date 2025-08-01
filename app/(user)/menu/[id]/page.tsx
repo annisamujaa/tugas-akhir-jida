abe445043ac467d3de6cd2889c78d8daac0ded88
annisamujaa <annisama2611@gmail.com>
2025-08-01 19:30:53 +0700

    edit menu

--- app/(user)/menu/[id]/page.tsx
+++ app/(user)/menu/[id]/page.tsx
@@ -3,13 +3,12 @@ import Image from 'next/image';
 import { notFound } from 'next/navigation';
 import QuantityClient from './quantityClient'; // komponen client-side
 
-interface Props {
-  params: { id: string };
-}
-
-export default async function MenuDetailPage({ params }: Props) {
-  const { id } = params; // ⛔ JANGAN pakai `await params`
-
+export default async function MenuDetailPage({
+  params,
+}: {
+  params: Promise<{ id: string }>; // 
+}) {
+  const { id } = await params;
   const menu = await prisma.menu.findUnique({
     where: { id },
   });
@@ -19,7 +18,7 @@ export default async function MenuDetailPage({ params }: Props) {
   return (
     <main className="min-h-screen bg-gray-50 py-10">
       <h1 className="text-2xl font-bold text-center mb-6">Menu Yang Kamu Pilih</h1>
-      
+
       <div className="max-w-3xl mx-auto bg-white px-6 py-3 rounded-lg shadow">
         <div className="flex flex-col md:flex-row gap-6 px-3 items-center">
           <div className="w-full md:w-1/3">
@@ -40,7 +39,6 @@ export default async function MenuDetailPage({ params }: Props) {
       </div>
 
       <div className="max-w-3xl mx-auto">
-        {/* Komponen Client-side: Quantity input & tombol tambah */}
         <QuantityClient price={menu.price} menuId={menu.id} />
       </div>
     </main>
