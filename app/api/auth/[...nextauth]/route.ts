ddba2785394c33577123daafd43a44502ae6c650
annisamujaa <annisama2611@gmail.com>
2025-08-01 20:38:03 +0700

    fix nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -113,3 +113,4 @@ import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
+export { authOptions };
--- app/api/user/menu/[id]/route.ts
+++ app/api/user/menu/[id]/route.ts
@@ -1,3 +1,60 @@
+// import { NextRequest, NextResponse } from 'next/server';
+// import { prisma } from '@/lib/prisma';
+
+// // GET menu by ID
+// export async function GET(
+//   req: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   const id = params.id;
+
+//   if (!id) {
+//     return NextResponse.json({ error: 'ID tidak ditemukan' }, { status: 400 });
+//   }
+
+//   try {
+//     const menu = await prisma.menu.findUnique({
+//       where: { id },
+//     });
+
+//     if (!menu) {
+//       return NextResponse.json({ error: 'Menu tidak ditemukan' }, { status: 404 });
+//     }
+
+//     return NextResponse.json(menu);
+//   } catch (error) {
+//     console.error('Gagal ambil menu:', error);
+//     return NextResponse.json({ error: 'Terjadi kesalahan server' }, { status: 500 });
+//   }
+// }
+
+// // PATCH update menu by ID
+// export async function PATCH(
+//   req: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   const id = params.id;
+
+//   if (!id) {
+//     return NextResponse.json({ error: 'ID tidak ditemukan' }, { status: 400 });
+//   }
+
+//   try {
+//     const body = await req.json();
+//     const { name, price, description, image } = body;
+
+//     const updatedMenu = await prisma.menu.update({
+//       where: { id },
+//       data: { name, price, description, image },
+//     });
+
+//     return NextResponse.json(updatedMenu);
+//   } catch (error) {
+//     console.error('Gagal update menu:', error);
+//     return NextResponse.json({ error: 'Gagal mengupdate produk' }, { status: 500 });
+//   }
+// }
+
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 import { URL } from 'url';
@@ -56,3 +113,4 @@ export async function PATCH(req: NextRequest) {
     return NextResponse.json({ error: 'Gagal mengupdate produk' }, { status: 500 });
   }
 }
+
